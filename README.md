176c4f027d65c62ea29fc8bd2bc2242dcec4c10c
ryuzo111 <yajima0401ryuzo@gmail.com>
2023-02-07 01:04:28 +0900

    feat

--- README.md
+++ README.md
@@ -14,3 +14,11 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter development, view the
 [online documentation](https://docs.flutter.dev/), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+
+## Shared Preferencesの使い方
+### できること
+Flutterで作成したアプリ内で、データを保存したり、読み出したりできるようになります。（ローカルに保存読み込むができる）
+アプリを閉じて再起動してもデータは残り続ける
+インストール
+```
+```
--- lib/redux/middleware/todos_middleware.dart
+++ lib/redux/middleware/todos_middleware.dart
@@ -3,3 +3,17 @@ import 'dart:math' as math;
 
 import 'package:redux/redux.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
+// import '../../models/models.dart';
+// import '../redux.dart';
+import '../../models/todos/todo.dart';
+
+const _todosJsonSharedPreferencesKey = 'todosJson';
+
+final List<Middleware<AppState>> todosMiddleware = [
+  TypedMiddleware<AppState, TodoAddAction>(((store, action, next) async{
+    final todosState = store.state.todosState;
+    final nextId = todosState.nextId;
+    final todos = <Todo>[Todo(id:nextId, name: action.name)];
+  }))
+]

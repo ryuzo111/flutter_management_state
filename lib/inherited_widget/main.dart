abba59c78b1bc30c270211f0ad4ca25380ccf135
ryuzo111 <yajima0401ryuzo@gmail.com>
2023-04-01 23:33:16 +0900

    状態管理

--- lib/inherited_widget/main.dart
+++ lib/inherited_widget/main.dart
@@ -1,27 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_management_state/provider/main.dart';
-import 'package:flutter_management_state/inherited_widget/main.dart';
-
-void main() {
-  runApp(const MyApp());
-  // runApp(const MyProviderApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
